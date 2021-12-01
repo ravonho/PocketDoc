84bb0c78cd0f6138126a5bb63eadeb846f128aa1
ravonho <hoyaolun36@gmail.com>
2021-12-01 21:04:12 +0800

    add activity

---
+++ app/src/main/java/com/example/pocketdoc/Myappointment.java
@@ -0,0 +1,14 @@
+package com.example.pocketdoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Myappointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myappointment);
+    }
+}
