248514b52319a576a236aca2ae7e6bedb0d41912
ravonho <hoyaolun36@gmail.com>
2021-12-10 12:23:12 +0800

    add doctor profile

--- app/src/main/java/com/example/pocketdoc/docprofile.java
+++ app/src/main/java/com/example/pocketdoc/docprofile.java
@@ -1,25 +1,20 @@
 package com.example.pocketdoc;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class dochomepage extends AppCompatActivity {
+public class docprofile extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -38,7 +33,10 @@ public class dochomepage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dochomepage);
+        setContentView(R.layout.activity_docprofile);
+        DrawerFunction();
+    }
+    public void DrawerFunction() {
         drawerLayout = findViewById(R.id.drawerlayout);
         navigationView = findViewById(R.id.navigationView);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.menu_Open, R.string.menu_CLose);
@@ -53,23 +51,23 @@ public class dochomepage extends AppCompatActivity {
                     case R.id.nav_home:
                         Log.i("MENU_DRAWER_TAG", "Home item is clicked");
                         drawerLayout.closeDrawer(GravityCompat.START);
+                        startActivity(new Intent(docprofile.this, dochomepage.class));
                         break;
 
                     case R.id.nav_profile:
                         Log.i("Menu_Drawer_Tag", "Profile item is clicked");
                         drawerLayout.closeDrawer(GravityCompat.START);
-                        startActivity(new Intent(dochomepage.this, docprofile.class));
                         break;
+
 
                     case R.id.nav_logout:
                         Log.i("Menu_Drawer_Tag", "Logout item is clicked");
                         drawerLayout.closeDrawer(GravityCompat.START);
-                        startActivity(new Intent(dochomepage.this, MainActivity.class));
+                        startActivity(new Intent(docprofile.this, MainActivity.class));
                         break;
                 }
                 return true;
             }
         });
-
     }
 }
