a58b3bbf1da4780f88fb65660a2763bb58db9e32
ravonho <hoyaolun36@gmail.com>
2021-12-10 22:20:35 +0800

    .

---
+++ app/src/main/java/com/example/pocketdoc/SearchHospital.java
@@ -0,0 +1,14 @@
+package com.example.pocketdoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchHospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_hospital);
+    }
+}
