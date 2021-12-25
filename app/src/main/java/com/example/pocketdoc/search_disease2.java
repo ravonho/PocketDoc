6df6b15c0ca695b818d20240aa62d0cb4431a3df
jonathannth <jonathannth21@gmail.com>
2021-12-25 22:49:43 +0800

    Added search_disease2 and search_doctor2.

---
+++ app/src/main/java/com/example/pocketdoc/search_disease2.java
@@ -0,0 +1,14 @@
+package com.example.pocketdoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class search_disease2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_disease2);
+    }
+}
