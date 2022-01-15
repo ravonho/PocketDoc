10a942bd92efd301147a94f79fc5ca89f34a4650
dunlikecoding <95513587+dunlikecoding@users.noreply.github.com>
2022-01-15 14:32:50 +0800

    Appointment can view all history

--- app/src/main/java/com/example/pocketdoc/Myappointment.java
+++ app/src/main/java/com/example/pocketdoc/Myappointment.java
@@ -1,59 +1,29 @@
 package com.example.pocketdoc;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.TextureView;
-import android.widget.TextView;
-
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.textfield.TextInputLayout;
-
-
-import static com.google.android.material.snackbar.Snackbar.make;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Parcelable;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 public class Myappointment extends AppCompatActivity {
@@ -61,19 +31,13 @@ public class Myappointment extends AppCompatActivity {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle actionBarDrawerToggle;
-
     TextInputLayout name, appointment_date, appointment_time,appointment_disease,appointment_hospital;
-
-//    TextInputLayout name, password, email, contact;
-    Button update, changepassword;
-    Spinner dropdown;
-    //private static final String[] items = {"Bernard", "JON", "Wong", "Yao Lun"};
+    RequestQueue queue;
+    Button nextRecord, previousRecord;
     String target = "";
-    String userpassword = "";
-    String beforeName = "default";
-    String beforeEmail = "default@hotmail.com";
-    String beforeContact = "9274010";
-    String beforePassword = "123456";
+    int appointment_id, idIndex;
+    ArrayList <Integer> idArray;
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
@@ -83,19 +47,31 @@ public class Myappointment extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void setCurrent(int curr){
+        appointment_id = curr;
+    }
+
+    public int getCurrent() {
+        return appointment_id;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myappointment);
         target = getIntent().getStringExtra("username");
-//        userpassword = getIntent().getStringExtra("password");
         Log.i("Username from intent", target);
-//        Log.i("Password from intent", userpassword);
+        queue = Volley.newRequestQueue(Myappointment.this);
+        idArray = new ArrayList<Integer>();
+        idIndex = 0;
         name = findViewById(R.id.username);
         appointment_date = findViewById(R.id.appointment_date);
         appointment_time = findViewById(R.id.appointment_time);
         appointment_hospital = findViewById(R.id.appointment_hospital);
         appointment_disease = findViewById(R.id.appointment_disease);
+
+        previousRecord = findViewById(R.id.previousRecord);
+        nextRecord = findViewById(R.id.nextRecord);
 
         drawerLayout = findViewById(R.id.drawerlayout);
         navigationView = findViewById(R.id.navigationView);
@@ -136,53 +112,124 @@ public class Myappointment extends AppCompatActivity {
             }
         });
 
-
-
         //Snackbar
         drawerLayout = findViewById(R.id.drawerlayout);
 
+        previousRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Previous Button Before", "onClick: %s".format(String.valueOf(previousRecord.isEnabled())));
+                Log.d("Previous Button Before", "current Index: ".concat(String.valueOf(idIndex)));
+                Log.d("Previous Button Before", "idArray size: ".concat(String.valueOf(idArray.size())));
+                if (!(idIndex - 1 < 0)) {
+                    previousRecord.setEnabled(true);
+                    nextRecord.setEnabled(true);
+                    idIndex = idIndex - 1;
+                    setCurrent(idArray.get(idIndex));
+                    getAppointmentByID(getCurrent());
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"No past appointments found!", Toast.LENGTH_LONG).show();
+                    previousRecord.setEnabled(false);
+                }
+                Log.d("Previous Button After", "onClick: %s".format(String.valueOf(previousRecord.isEnabled())));
+                Log.d("Previous Button After", "current Index: ".concat(String.valueOf(idIndex)));
+            }
+        });
 
+        nextRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Next Button Before", "onClick: %s".format(String.valueOf(nextRecord.isEnabled())));
+                Log.d("Next Button Before", "current Index: ".concat(String.valueOf(idIndex)));
+                Log.d("Next Button Before", "idArray size: ".concat(String.valueOf(idArray.size())));
+                if (!(idIndex + 1 > idArray.size() - 1)) {
+                    nextRecord.setEnabled(true);
+                    previousRecord.setEnabled(true);
+                    idIndex = idIndex + 1;
+                    setCurrent(idArray.get(idIndex));
+                    getAppointmentByID(getCurrent());
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"This is the latest appointment!", Toast.LENGTH_LONG).show();
+                    nextRecord.setEnabled(false);
+                }
+                Log.d("Next Button After", "onClick: %s".format(String.valueOf(nextRecord.isEnabled())));
+                Log.d("Next Button After", "current Index: ".concat(String.valueOf(idIndex)));
+            }
+        });
 
+        doGetAppointment();
+    }
 
-        //dropdown
-//        dropdown = findViewById(R.id.spinner1);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        dropdown.setAdapter(adapter);
-//        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-//                target = adapterView.getItemAtPosition(i).toString();
-//                beforeName = target;
-//                Toast toast = Toast.makeText(getApplicationContext(), "THIS IS ".concat(target), Toast.LENGTH_LONG);
-//                toast.setGravity(Gravity.CENTER, 0, 30);
-//                toast.show();
-//                Snackbar snackbar1 = make(drawerLayout, "Message is restored!", Snackbar.LENGTH_SHORT);
-//                snackbar1.show();
-//                doGetByUser(target);
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> adapterView) {
-//                Toast toast = Toast.makeText(getApplicationContext(), "NOTHING IS SELECTED ".concat(target), Toast.LENGTH_LONG);
-//                toast.setGravity(Gravity.CENTER, 0, 30);
-//                toast.show();
-//            }
-//        });
-//
+    private void doGetAppointment() {
+        doGetAppointmentIDByName(target);
+        new java.util.Timer().schedule(new java.util.TimerTask() {
+            @Override
+            public void run() {
+                Log.d("current value", String.valueOf(getCurrent()));
+                getAppointmentByID(getCurrent());
+            }
+        }, 1000);
+    }
 
-        doGetAppointment(target);
-}
-
-    private void doGetAppointment(String username) {
-
-        RequestQueue queue = Volley.newRequestQueue(Myappointment.this);
-        String url ="https://pocket-dr.herokuapp.com/index.php?action=showAppointment&username=".concat(username);
+    private void doGetAppointmentIDByName(String username) {
+        String url ="https://pocket-dr.herokuapp.com/index.php?action=getAppointmentIDByName&username=".concat(username);
 
         // Request a string response from the provided URL.
         final Map<String, String> params = new HashMap<String, String>();
-        params.put("action", "showAppointment");
+        params.put("action", "getAppointmentIDByName");
         params.put("username", target);
-        Log.d("anything", "onClick: getParams ".concat(params.toString()));
+        Log.d("getAppointmentIDByName", "onClick: getParams ".concat(params.toString()));
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("response", "onResponse: ".concat(response));
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            JSONArray array = object.getJSONArray("results");
+                            int row = object.getInt("RowCount");
+                            Log.d("GAID array", array.toString());
+                            Log.d("GAID rowcount", String.valueOf(row));
+                            for (int i = 0; i<array.length();i++){
+                                JSONObject tmpobject = array.getJSONObject(i);
+                                idArray.add(tmpobject.getInt("idAppointment"));
+                                Log.d("get id for loop", String.valueOf(idArray.get(i)));
+                            }
+                            setCurrent(idArray.get(0));
+                            Log.d("after getID curr value", String.valueOf(getCurrent()));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        // Display the first 500 characters of the response string.
+                        Log.d("Volley Reponse", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() { return params; }
+        };
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    private void getAppointmentByID(int id) {
+        Log.d("passed in id value", String.valueOf(id));
+        String url ="https://pocket-dr.herokuapp.com/index.php?action=getAppointmentByID&idAppointment=".concat(String.valueOf(id));
+
+        // Request a string response from the provided URL.
+        final Map<String, String> params = new HashMap<String, String>();
+        params.put("action", "getAppointmentByID");
+        params.put("idAppointment", String.valueOf(id));
+        Log.d("getAppointmentByID", "onClick: getParams ".concat(params.toString()));
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
@@ -233,4 +280,4 @@ public class Myappointment extends AppCompatActivity {
 // Add the request to the RequestQueue.
         queue.add(stringRequest);
     }
-    }
+}
